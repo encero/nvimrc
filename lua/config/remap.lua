57ca276fd88e28754a8d773b52d4d9bc0a052a35
mmichalik_pure <mmichalik@purestorage.com>
2024-05-22 17:05:40 +0200

    fix: harpoon and jump to pane bindings collision

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,10 +3,10 @@ vim.keymap.set('i', 'jk', '<esc>', { silent = true })
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<M-j>", "<C-w>j")
-vim.keymap.set("n", "<M-k>", "<C-w>k")
-vim.keymap.set("n", "<M-h>", "<C-w>h")
-vim.keymap.set("n", "<M-l>", "<C-w>l")
+vim.keymap.set("n", "<M-J>", "<C-w>j")
+vim.keymap.set("n", "<M-K>", "<C-w>k")
+vim.keymap.set("n", "<M-H>", "<C-w>h")
+vim.keymap.set("n", "<M-L>", "<C-w>l")
 
 vim.keymap.set("n", "<M-<>", "5<C-w><")
 vim.keymap.set("n", "<M->>", "5<C-w>>")
