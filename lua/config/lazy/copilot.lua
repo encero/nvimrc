faeb8ab3d635a8298855c08aa971b0671a0ae6c0
mmichalik_pure <mmichalik@purestorage.com>
2024-07-16 10:08:05 +0200

    configure copilot completion key

--- lua/config/lazy/copilot.lua
+++ lua/config/lazy/copilot.lua
@@ -1,3 +1,9 @@
 return {
     "github/copilot.vim",
+    config = function()
+        vim.keymap.set("i", "<C-i>", 'copilot#Accept("\\<CR>")',
+            { expr = true, replace_keycodes = false })
+
+        vim.g.copilot_no_tab_map = true
+    end
 }
