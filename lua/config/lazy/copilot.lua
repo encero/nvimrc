6225a542b9aad47f582da6ad32b9af1a3929cb41
mmichalik_pure <mmichalik@purestorage.com>
2024-07-22 09:38:11 +0200

    trying to get a functional copilot map on remote machine with broken term

--- lua/config/lazy/copilot.lua
+++ lua/config/lazy/copilot.lua
@@ -1,7 +1,7 @@
 return {
     "github/copilot.vim",
     config = function()
-        vim.keymap.set("i", "<C-i>", 'copilot#Accept("\\<CR>")',
+        vim.keymap.set("i", "<C-;>", 'copilot#Accept("\\<CR>")',
             { expr = true, replace_keycodes = false })
 
         vim.g.copilot_no_tab_map = true
