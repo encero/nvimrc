198e0677f1ed62b18fc2580314fabc0bf86206b6
mmichalik_pure <mmichalik@purestorage.com>
2025-05-14 11:21:22 +0000

    augment code added

---
+++ lua/config/lazy/augment.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'augmentcode/augment.vim',
+        init = function()
+            vim.g.augment_workspace_folders = { "~/iridium" }
+            vim.g.augment_disable_tab_mapping = true
+        end,
+        config = function(opts)
+            -- require("augment").setup(opts)
+
+            vim.keymap.set("i", "<C-_>", '<cmd>call augment#Accept()<CR>', { desc = "Accept augment suggestion" })
+            vim.keymap.set({ "n", "v" }, "<leader>ic", "<cmd>Augment chat<CR>", { desc = "Open augment chat" })
+            vim.keymap.set({ "n", "v" }, "<leader>it", "<cmd>Augment chat-toggle<CR>", { desc = "Toggle augment chat" })
+        end,
+    },
+}
