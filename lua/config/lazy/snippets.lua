1a1b80f9330e6a700173f314c42c7345e80a2aec
mmichalik_pure <mmichalik@purestorage.com>
2025-07-10 16:06:45 +0200

    some jira snippets

--- lua/config/lazy/snippets.lua
+++ lua/config/lazy/snippets.lua
@@ -11,6 +11,16 @@ return {
         config = function()
             local ls = require("luasnip")
 
+            local s = ls.snippet
+            local sn = ls.snippet_node
+            local isn = ls.indent_snippet_node
+            local t = ls.text_node
+            local i = ls.insert_node
+            local f = ls.function_node
+            local c = ls.choice_node
+            local d = ls.dynamic_node
+            local r = ls.restore_node
+
             vim.keymap.set({ 'i', 's' }, '<m-j>', function()
                 if ls.expand_or_jumpable() then
                     ls.expand_or_jump()
@@ -28,6 +38,16 @@ return {
                     ls.change_choice(1)
                 end
             end)
+
+            ls.add_snippets("all", {
+                s("nojira", {
+                    t "JIRA: NO-JIRA ", i(1, "description"),
+                }),
+                s("jira", {
+                    t "JIRA: ", i(1, "IR-xxx"), t" ", i(2, "description"),
+                }),
+            })
+
         end,
     }
 }
