4ae69359055cc6c9d41e45df309be083e2dc4bbf
mmichalik_pure <mmichalik@purestorage.com>
2025-07-10 15:59:36 +0200

    fix augment keymap on mac

--- lua/config/lazy/augment.lua
+++ lua/config/lazy/augment.lua
@@ -9,6 +9,7 @@ local augment = {
             -- require("augment").setup(opts)
 
             vim.keymap.set("i", "<C-_>", '<cmd>call augment#Accept()<CR>', { desc = "Accept augment suggestion" })
+            vim.keymap.set("i", "<C-/>", '<cmd>call augment#Accept()<CR>', { desc = "Accept augment suggestion" })
             vim.keymap.set({ "n", "v" }, "<leader>ic", "<cmd>Augment chat<CR>", { desc = "Open augment chat" })
             vim.keymap.set({ "n", "v" }, "<leader>it", "<cmd>Augment chat-toggle<CR>", { desc = "Toggle augment chat" })
         end,
